704583811e9d8b8e196594fb787afa71b0a9a833
Felipe Arruda <felipearrudajf@gmail.com>
2021-04-18 20:26:39 -0300

    Deleção do código

--- ping.py
+++ ping.py
@@ -1,20 +1,4 @@
-import schedule
-import time
-import speedtest
-from datetime import datetime
-import pandas as pd
-import numpy as numpy
-from threading import Timer
+def oi():
+    print('Olha')
 
-
-def internet():
-    df = []
-    s = speedtest.SpeedTest()
-    data_atual = datetime.now().strftime('%d/%m/%Y')
-    hora_atual = datetime.now().strftime('%H:%M')
-    velocidade = s.download(threads=None)*(10**-6)
-    df.loc[len(df)] = [data_atual, hora_atual, velocidade]
-    df.to_excel('dados.xlsx', sheet_name='base', index=False)
-    Timer(1800, internet).start()
-
-internet()
+    
