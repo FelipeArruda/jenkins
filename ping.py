28eef4d77ee5a4e655517cd6bc056b9e21257bb0
Felipe Arruda <felipearrudajf@gmail.com>
2021-04-18 20:37:59 -0300

    Reverse

--- ping.py
+++ ping.py
@@ -1,4 +1,20 @@
-def oi():
-    print('Olha')
+import schedule
+import time
+import speedtest
+from datetime import datetime
+import pandas as pd
+import numpy as numpy
+from threading import Timer
 
-    
+
+def internet():
+    df = []
+    s = speedtest.SpeedTest()
+    data_atual = datetime.now().strftime('%d/%m/%Y')
+    hora_atual = datetime.now().strftime('%H:%M')
+    velocidade = s.download(threads=None)*(10**-6)
+    df.loc[len(df)] = [data_atual, hora_atual, velocidade]
+    df.to_excel('dados.xlsx', sheet_name='base', index=False)
+    Timer(1800, internet).start()
+
+internet()
